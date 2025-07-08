d9df530524c9ca469dfe6cc4ad073bc3b4046e31
Ritikraj3 <ritikraj5776@gmail.com>
2025-07-08 18:40:49 +0530

    loop revised

---
+++ Warm-Up/Loop-1/script.js
@@ -0,0 +1,20 @@
+for (let i = 0; i <= 10; i++) {
+    console.log("Ritik raj");
+}
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr.length);
+console.log(arr[0]);
+
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 == 0){
+        console.log(arr[i]);
+    }
+    
+}
+
+let i = 0;
+while (i < 5) {
+    console.log("Ritik");
+    i++;
+}
