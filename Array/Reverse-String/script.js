dd0d283bd89518449739326871ab26454d1f3646
Ritikraj3 <ritikraj5776@gmail.com>
2025-07-14 20:20:57 +0530

    reverse string

---
+++ Array/Reverse-String/script.js
@@ -0,0 +1,14 @@
+let arr = ["h", "e", "l", "l", "o"];
+
+function reverseString(arr) {
+    let len = arr.length
+    let halfLen = Math.floor(len / 2);
+    for (let i = 0; i < halfLen; i++) {
+        let temp = arr[i];
+        arr[i] = arr[len - 1 - i];
+        arr[len - 1 - i] = temp;
+    }
+    return arr
+}
+
+console.log(reverseString(arr));
