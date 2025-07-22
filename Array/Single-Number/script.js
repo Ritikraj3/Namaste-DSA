1295aee4b526576d93c0e4b771cac99f417fe866
Ritikraj3 <ritikraj5776@gmail.com>
2025-07-23 01:25:41 +0530

    single no. with xor

--- Array/Single-Number/script.js
+++ Array/Single-Number/script.js
@@ -1,11 +1,34 @@
 function singleNumber(arr) {
-  let result = 0;
-  for (let num of arr) {
-    result ^= num; // XOR cancels out duplicate numbers
+  let x = 0;
+  for (let i = 0; i < arr.length; i++) {
+    x ^= arr[i];
   }
-  return result;
+  return x;
 }
 
 console.log(singleNumber([2, 3, 5, 4, 5, 3, 4])); 
 
 // Time Complexity: O(n)
+
+
+//hash map approach
+
+function singleNo(nums) {
+   let hash = {}
+
+   for (let i = 0; i < nums.length; i++) {
+       if (!hash[nums[i]]) {
+          hash[nums[i]] = 1
+       } else {
+           hash[nums[i]]++
+       }
+   }
+   for(let i = 0; i < nums.length; i++) {
+       if (hash[nums[i]] == 1) {
+           return nums[i]
+       }
+   }
+   
+}
+
+console.log(singleNo([2, 3, 5, 4, 5, 3, 4]));
