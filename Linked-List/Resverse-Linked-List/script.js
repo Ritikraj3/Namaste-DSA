4bce76665cf9f2377170bddd291b8b21a0207942
Ritikraj3 <ritikraj5776@gmail.com>
2025-08-03 20:45:59 +0530

    Reverse Linked List

---
+++ Linked-List/Resverse-Linked-List/script.js
@@ -0,0 +1,13 @@
+function reverse(head) {
+    let prev = null
+    let curr = head
+
+    while(curr) {
+        let temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
+    }
+    head = prev
+    return head
+}
