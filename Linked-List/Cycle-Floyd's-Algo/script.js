0b75898e3d0ea4a80a7dd0ad3fd03887842698a5
Ritikraj3 <ritikraj5776@gmail.com>
2025-08-06 12:31:22 +0530

    floyd's to check cycle

---
+++ Linked-List/Cycle-Floyd's-Algo/script.js
@@ -0,0 +1,12 @@
+function floydFinds(head) {
+    if(!head) return false
+    let slow = head
+    let fast = head.next
+
+    while (slow != fast) {
+        if (fast == null || fast.next == null) return false
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return true
+}
