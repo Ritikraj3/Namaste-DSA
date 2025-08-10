b9c559c80e245c9c64edd56db0ff0a71dfd46d98
Ritikraj3 <ritikraj5776@gmail.com>
2025-08-10 23:41:01 +0530

    swap nodes in pair

---
+++ Linked-List/Swap-Nodes-in-Pair/script.js
@@ -0,0 +1,19 @@
+function swapNodesInPair (head) {
+    if (!head || !head.next) return head
+
+    let dummy = new ListNode()
+    let p = dummy
+    let c = head
+    let n = head.next
+
+    while(c && n) {
+        p.next = n
+        c.next = n.next
+        n.next = c
+
+        p = c
+        c = p.next
+        n = c && c.next
+    }
+    return dummy.next
+}
