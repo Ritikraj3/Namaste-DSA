e7f3d342471ec4781fb6e19a49ab6ca66333cdca
Ritikraj3 <ritikraj5776@gmail.com>
2025-08-12 02:05:33 +0530

    added another approach

--- Strings/Length-of-last-word/script.js
+++ Strings/Length-of-last-word/script.js
@@ -1,3 +1,4 @@
+//Approach 1 :----
 function lengthOfLastWord(s) {
     let n = s.length - 1
 
@@ -28,3 +29,22 @@ function lengthOfLastWord(s) {
 }
 
 console.log(lengthOfLastWord("  ok  i am coming  "));
+
+//Approach 2:---
+
+function lastWord(s) {
+    let n = s.length-1
+    let count = 0
+
+    while(n>=0) {
+        if(s[n] !== " "){
+            count++
+        } else if ( count > 0){
+            break
+        }
+        n--
+    }
+    return count
+}
+
+console.log(lastWord("  Ritik  Raj   "));
