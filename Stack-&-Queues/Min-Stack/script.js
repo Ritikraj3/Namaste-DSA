3ac6941a0c5610224335fa89142fbf4ab7409e51
Ritikraj3 <ritikraj5776@gmail.com>
2025-08-20 17:32:53 +0530

    Min Stack

---
+++ Stack-&-Queues/Min-Stack/script.js
@@ -0,0 +1,45 @@
+class MyStack {
+    constructor() {
+        this.s = []
+    }
+
+    push(val) {
+        if(this.s.length === 0) {
+            this.s.push([val,val])
+        } else {
+            let lastMin = this.s[this.s.length-1][1]
+            let newMin = Math.min(val , lastMin)
+            this.s.push([val,newMin])
+        }
+    }
+
+    pop() {
+        this.s.pop()
+    }
+
+    top() {
+        return this.s[this.s.length-1][0]
+    }
+
+    getMin() {
+        return this.s[this.s.length-1][1]
+    }
+
+}
+
+let st = new MyStack();
+
+st.push(5);   
+st.push(3);   
+st.push(7);   
+
+console.log(st.getMin()); // 3
+
+st.pop();     
+console.log(st.getMin()); // 3
+
+st.pop();     
+st.push(1)
+console.log(st.getMin()); // 1
+
+console.log(st);  // to see the internal stack
