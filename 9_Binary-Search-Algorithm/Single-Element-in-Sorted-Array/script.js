f33cfc04990458ce377e505355f16a0ebfca66c1
Ritikraj3 <ritikraj5776@gmail.com>
2025-09-03 19:13:40 +0530

    single element in sorted array

---
+++ 9_Binary-Search-Algorithm/Single-Element-in-Sorted-Array/script.js
@@ -0,0 +1,28 @@
+function singleNonDuplicate (arr) {
+    let l = 0
+    let r = arr.length-1
+
+    while(l <= r) {
+        let m = l + Math.floor((r - l)/2) 
+        if(arr[m] !== arr[m+1] && arr[m] !== arr[m-1]) return arr[m]
+
+        if(arr[m] == arr[m-1]) {
+            leftCount = (m-1)-l
+            if(leftCount % 2 == 1) {
+                r = m-2
+            } else {
+                l = m+1
+            }
+        } else {
+            leftCount = m-l
+            if(leftCount % 2 == 1) {
+                r = m - 1
+            } else {
+                l = m + 2
+            }
+        }
+        return arr[r]
+    }
+}
+
+console.log(singleNonDuplicate([1,1,2,3,3,4,4,8,8]));
