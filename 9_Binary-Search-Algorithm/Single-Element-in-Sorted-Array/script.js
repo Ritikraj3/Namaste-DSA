9cbc689051305413916aa9fb8b89559012f25d7d
Ritikraj3 <ritikraj5776@gmail.com>
2025-09-04 12:45:17 +0530

    single element in sorted array

--- 9_Binary-Search-Algorithm/Single-Element-in-Sorted-Array/script.js
+++ 9_Binary-Search-Algorithm/Single-Element-in-Sorted-Array/script.js
@@ -6,7 +6,9 @@ function singleNonDuplicate (arr) {
         let m = l + Math.floor((r - l)/2) 
         if(arr[m] !== arr[m+1] && arr[m] !== arr[m-1]) return arr[m]
 
+        //if pair exists om left side
         if(arr[m] == arr[m-1]) {
+            //if odd no. of elements on leftside -->(single element lies here!)
             leftCount = (m-1)-l
             if(leftCount % 2 == 1) {
                 r = m-2
