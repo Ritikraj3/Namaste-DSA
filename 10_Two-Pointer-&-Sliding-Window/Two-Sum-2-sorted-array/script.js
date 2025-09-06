ee46f2e77c96c65fe911012d42198c6424f96572
Ritikraj3 <ritikraj5776@gmail.com>
2025-09-06 23:39:01 +0530

    two sum - when array is sorted

---
+++ 10_Two-Pointer-&-Sliding-Window/Two-Sum-2-sorted-array/script.js
@@ -0,0 +1,16 @@
+ function twoSum (numbers, target) {
+    let i = 0
+    let j = numbers.length - 1
+
+    while (i < j) {
+        let sum = numbers[i] + numbers[j]
+        if (sum == target) {
+            return [i + 1, j + 1]
+        } else if (sum > target) {
+            j--
+        } else {
+            i++
+
+        }
+    }
+};
